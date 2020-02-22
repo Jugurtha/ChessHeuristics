a4747ebd1deefe1b92fa954493349f7f9b322e13
Jugurtha <Jugurtha.2009@gmail.com>
2020-02-22 21:55:10 +0100

    Done.

---
+++ cmake-build-debug/CMakeFiles/TP3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP3.dir/jeu.c.obj"
+  "TP3.pdb"
+  "TP3.exe"
+  "TP3.exe.manifest"
+  "libTP3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
